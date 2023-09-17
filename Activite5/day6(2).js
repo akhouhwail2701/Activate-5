b09bda17551296d2c709f8fe51fa63b593178203
akhouhwail2701 <102318140+akhouhwail2701@users.noreply.github.com>
2023-09-17 10:31:45 -0700

    Add files via upload

---
+++ Activite5/day6(2).js
@@ -0,0 +1,8 @@
+document.getElementById("btn").onclick = function () {
+    if (document.getElementById("menu").style.visibility == "hidden") {
+        document.getElementById("menu").style.visibility = "visible"
+    }
+    else {
+        document.getElementById("menu").style.visibility = "hidden"
+    }
+}
